9f331f51248aee982d590392a7d195780ae2a52b
GregRL94 <largreg1994@gmail.com>
2024-03-11 13:46:03 -0400

    Added .plastic to gitignore, preparing for merge from main

--- Assets/Scripts/UI/LevelCounter.cs
+++ Assets/Scripts/UI/LevelCounter.cs
@@ -1,17 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class LevelCounter : MonoBehaviour
 {
+    [SerializeField] private TMP_Text textDisplay;
+
     // Start is called before the first frame update
     void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
     {
         
     }
