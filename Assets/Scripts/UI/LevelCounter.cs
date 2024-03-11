2243772586e2012e20479bc8567ca4186cbbc801
GregRL94 <largreg1994@gmail.com>
2024-03-11 14:51:25 -0400

    Removed plasticSCM from tracked files, implemented level indicator

--- Assets/Scripts/UI/LevelCounter.cs
+++ Assets/Scripts/UI/LevelCounter.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 
@@ -10,6 +8,7 @@ public class LevelCounter : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        string[] rawTxt = GameManager.CurrentScene.ToString().Split("_");
+        textDisplay.text = rawTxt[1];
     }
 }
